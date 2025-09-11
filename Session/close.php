46b34febbf3703c48c8199d601d551f490d4c1d5
Heath Boutilier <heathboutilier@outlook.com>
2025-09-10 17:59:41 -0700

    Quick ease-of-use fix

--- Session/close.php
+++ Session/close.php
@@ -9,7 +9,11 @@ require_once __DIR__ . "/../Header/redirect.php";
  * cookies, then redirects user to login.
  * @return never
  */
-function close($destination = "/login"): never {
+function close($destination = "/login.php"): never {//! Had to add ".php" because there isn't a guaruntee a deployment of the library will use htaccess for extension stripping
+    if (session_status() == PHP_SESSION_NONE){
+        require_once __DIR__ . "/open.php";//! Want to do more conditional includes in the library where possible. Optimally, I want to avoid including anything unneccesary. Hence the separation of functions across files...
+        \crow\Session\open();
+    }
     $_SESSION = [];
     session_destroy();
     //! ... xAuth line rel. PHPEXP (value unclear)
